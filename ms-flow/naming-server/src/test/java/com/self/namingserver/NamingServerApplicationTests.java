2a9b57d5fdb496f50cab5835ee97a93462b023bf
Ayush <ayush@quovantis.com>
2021-02-26 12:20:44 +0530

    Added eureka naming server

---
+++ ms-flow/naming-server/src/test/java/com/self/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.self.namingserver;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
